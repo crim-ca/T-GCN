17294505efa0eec090b8ba041789a0cd94afd623
Luis Da Costa <dacosta.le@gmail.com>
2019-05-21 15:22:46 -0400

    Factorizes 'saving plot' code

--- visualization.py
+++ visualization.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
+import os
 import matplotlib.pyplot as plt
+
+FIG_EXT = ".png"
+
+
+def save_figure(path, filename_base):
+    file_name = os.path.join(path, filename_base + FIG_EXT)
+    plt.savefig(file_name)
+    print("Figure saved as '%s'" % (file_name))
+
 
 def plot_result(test_result,test_label1,path):
     ##all test result visualization
@@ -12,7 +22,9 @@ def plot_result(test_result,test_label1,path):
     plt.plot(a_pred,'r-',label='prediction')
     plt.plot(a_true,'b-',label='true')
     plt.legend(loc='best',fontsize=10)
-    plt.savefig(path+'/test_all.jpg')
+    plt.title('Results on full dataset')
+    plt.xlabel("Minutes (one point per 15')")
+    save_figure(path, 'test_all')
     plt.show()
     ## oneday test result visualization
     fig1 = plt.figure(figsize=(7,1.5))
@@ -22,47 +34,50 @@ def plot_result(test_result,test_label1,path):
     plt.plot(a_pred,'r-',label="prediction")
     plt.plot(a_true,'b-',label="true")
     plt.legend(loc='best',fontsize=10)
-    plt.savefig(path+'/test_oneday.jpg')
+    plt.title('Results on first day')
+    plt.xlabel("Minutes (one point per 15')")
+    save_figure(path, 'test_oneday')
     plt.show()
-    
+
+
 def plot_error(train_rmse,train_loss,test_rmse,test_acc,test_mae,path):
     ###train_rmse & test_rmse 
     fig1 = plt.figure(figsize=(5,3))
     plt.plot(train_rmse, 'r-', label="train_rmse")
     plt.plot(test_rmse, 'b-', label="test_rmse")
     plt.legend(loc='best',fontsize=10)
-    plt.savefig(path+'/rmse.jpg')
+    save_figure(path, 'rmse')
     plt.show()
     #### train_loss & train_rmse
     fig1 = plt.figure(figsize=(5,3))
     plt.plot(train_loss,'b-', label='train_loss')
     plt.legend(loc='best',fontsize=10)
-    plt.savefig(path+'/train_loss.jpg')
+    save_figure(path, 'train_loss')
     plt.show()
 
     fig1 = plt.figure(figsize=(5,3))
     plt.plot(train_rmse,'b-', label='train_rmse')
     plt.legend(loc='best',fontsize=10)
-    plt.savefig(path+'/train_rmse.jpg')
+    save_figure(path, 'train_rmse')
     plt.show()
 
     ### accuracy
     fig1 = plt.figure(figsize=(5,3))
     plt.plot(test_acc, 'b-', label="test_acc")
     plt.legend(loc='best',fontsize=10)
-    plt.savefig(path+'/test_acc.jpg')
+    save_figure(path, 'test_acc')
     plt.show()
     ### rmse
     fig1 = plt.figure(figsize=(5,3))
     plt.plot(test_rmse, 'b-', label="test_rmse")
     plt.legend(loc='best',fontsize=10)
-    plt.savefig(path+'/test_rmse.jpg')
+    save_figure(path, 'test_rmse')
     plt.show()
     ### mae
     fig1 = plt.figure(figsize=(5,3))
     plt.plot(test_mae, 'b-', label="test_mae")
     plt.legend(loc='best',fontsize=10)
-    plt.savefig(path+'/test_mae.jpg')
+    save_figure(path, 'test_mae')
     plt.show()
 
 
