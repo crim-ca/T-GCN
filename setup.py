6b9bd18ce7f8f3c8e44e8d7af4443908f541ca7c
Luis Da Costa <dacosta.le@gmail.com>
2019-06-12 14:01:48 -0400

    Packing, à la https://packaging.python.org/tutorials/packaging-projects/

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="crim-T-GCN",
+    version="0.0.1",
+    author="Luis Da Costa (for CRIM)",
+    author_email="luis.dacosta@crim.ca",
+    description="Fork of T-GCN",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/crim-ca/T-GCN",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
